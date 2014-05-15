01c8db68bda60127cc1a4d9d0462719e8a5c1c27
Admin <administrator@Mech-1.(none)>
2014-05-15 12:13:39 -0400

    <working on building all cases

--- Parent_OpenMP_Crystal_Level/Tests and old Versions/ompMaxThreads.f90
+++
@@ -1,6 +0,0 @@
-
-program ompMaxThreads 
-implicit none 
-integer :: omp_get_max_threads 
-print *, omp_get_max_threads() 
-end program 
---
+++ Spectral_OpenMP/gnuplot.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/gnuplot
+
+#runs commands for gnuplot
+reset
+
+# Fit data
+f(x) = m*x+b
+fit f(x) 'timing_TR1024.txt' using 1:2 via m,b
+
+set terminal png
+set xlabel "Number of Crystals"
+set ylabel "Time (Seconds)"
+set title "Spectral OMP Scaling"  
+set term png
+set output "Spectral_OMP_Scaling.png"
+                      
+set ytic auto           # set ytics automatically
+set xtic auto		# set xtics automatically
+#set logscale y 10
+#set logscale x 2
+set key left top
+plot "timing_TR1024.txt" using 1:2 with linespoints title 'Timing', f(x) title 'f=m*x'
+
+
